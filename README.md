350a21b5790a6b1004d7084d2f2d907dc68c0287
dl14 <dev14@vt.edu>
2017-11-28 14:28:59 -0500

    add dictionary code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gujarati English Dictionary
+Dictionary to go from Gujarati -> English
+
+Instructions:
+Run "python app.py" in the directory
+Access the webpage shown on the terminal (127.0.0.1:5000) and add /mainpage to the end of the URL
+Enter words IN GUJARATI!!
+Example words to try out:
+દેવ
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+import glexi
+
+app = Flask(__name__)
+print("IN APP.PY")
+@app.route('/mainpage', methods=['POST', 'GET'])
+def hello_world():
+    searchword = request.form.get('searchword')
+    print("calling glexi")
+    print(searchword)
+    def_arr = glexi.scrape_definition(searchword)
+    print("called glexi")
+    return render_template('main.html', searchword=searchword, def_arr=def_arr)
+
+if __name__ == "__main__":
+    app.run()
