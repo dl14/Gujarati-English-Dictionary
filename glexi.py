fae48c34465706d8a7fca12dfabad1f349eea927
dl14 <dev14@vt.edu>
2017-11-29 15:30:39 -0500

    updated dependencies

--- glexi.py
+++ glexi.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
+#from selenium import webdriver
 from bs4 import BeautifulSoup
 
-from selenium.webdriver.common.by import By
+'''from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException'''
 
 #import unittest, time, re
 import urllib
